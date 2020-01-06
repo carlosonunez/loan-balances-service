3f84c94f5f754f3a55a22f8634717f5afe27c71d
Carlos Nunez <carlosonunez@noreply.github.com>
2020-01-05 23:06:29 -0600

    Add API Gateway helpers

---
+++ lib/loan_balances_service/aws_helpers/api_gateway.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+require 'json'
+
+module LoanBalancesService
+  module AWSHelpers
+    module APIGateway
+      module Events
+        def self.get_param(event:, param:)
+          event.dig('queryStringParameters', param)
+        end
+
+        def self.get_access_key(event)
+          event['requestContext']['identity']['apiKey']
+        end
+      end
+
+      def self.get_endpoint(event, path_to_remove: '/auth')
+        # TODO: Fix TypeError Hash into String errror from API Gateway.
+        path = event['requestContext']['path'] || raise('Path not found.')
+        path_subbed = path.gsub!(path_to_remove, '')
+        host = event['headers']['Host'] || raise('Host not found in event.')
+        "#{host}#{path_subbed}"
+      end
+
+      def self.ok(message: nil, additional_json: nil)
+        if message.nil? && additional_json.nil?
+          return_200(json: { status: 'ok' })
+        elsif !message.nil?
+          return_200(json: { status: 'ok', message: message })
+        else
+          json = { status: 'ok' }.merge(additional_json)
+          return_200(json: json)
+        end
+      end
+
+      def self.error(message:)
+        return_422(body: message)
+      end
+
+      def self.not_found(message:)
+        return_404(body: message)
+      end
+
+      def self.unauthenticated(message: nil)
+        if message.nil?
+          return_403(body: 'Access denied.')
+        else
+          return_403(body: message)
+        end
+      end
+
+      def self.send_response(code:, payload:)
+        if !payload.nil? && (payload.class != Hash)
+          raise 'Payload must be a Hash'
+        end
+
+        {
+          statusCode: code,
+          body: payload.to_json
+        }
+      end
+
+      def self.return_200(body: nil, json: {})
+        raise "JSON can't be empty" if body.nil? && json.empty?
+
+        if !json.empty?
+          send_response(code: 200, payload: json)
+        else
+          send_response(code: 200, payload: { message: body })
+        end
+      end
+
+      def self.return_403(body:)
+        send_response(code: 403, payload: { status: 'error', message: body })
+      end
+
+      def self.return_404(body:)
+        send_response(code: 404, payload: { status: 'error', message: body })
+      end
+
+      def self.return_422(body:)
+        send_response(code: 422, payload: { status: 'error', message: body })
+      end
+
+      private_class_method :return_422
+      private_class_method :return_404
+      private_class_method :return_403
+      private_class_method :return_200
+      private_class_method :send_response
+    end
+  end
+end
