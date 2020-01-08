048e22cbda710de22ccc6a569e861efa2b673d2f
Carlos Nunez <carlosonunez@noreply.github.com>
2020-01-08 00:07:39 -0600

    Begin work on getting this working in Lambda.

---
+++ spec/integration/providers/datcu_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "Given a DATCU online account" do
+  context "When I log in" do
+    context "And I search for auto loans" do
+      example "Then I get an auto loan", :wip do
+      end
+    end
+  end
+end
