3c256ddb47fb132b0c274988255bc9a41e0b1e74
Carlos Nunez <carlosonunez@noreply.github.com>
2020-01-08 23:47:50 -0600

    squash

--- spec/integration/providers/datcu_spec.rb
+++ spec/integration/providers/datcu_spec.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 # I wrote this so that it only gets auto loans. If we support more types
 # of loans in the future, we are going to want to re-organize our providers
 # to support that and have libraries for common routines like logging in and
 # moving around.
-describe "Given a DATCU online account" do
-  context "When I log in" do
-      example "Then I get a balance", :integration do
-        params = { provider: 'datcu' }
-        Service.get('/balance',
-                    params: params,
-                    authenticated: true) do |response|
-          status = JSON.parse(response.body)[:status]
-          balance = JSON.parse(response.body)[:message]
-          expect(response.code).to eq 200
-          expect(status).to eq 'ok'
-          expect(balance).to match(/^\$[0-9,]{1}/)
-        end
+describe 'Given a DATCU online account' do
+  context 'When I log in' do
+    example 'Then I get a balance', :integration do
+      params = { provider: 'datcu' }
+      Service.get('/balance',
+                  params: params,
+                  authenticated: true) do |response|
+        status = JSON.parse(response.body)[:status]
+        balance = JSON.parse(response.body)[:message]
+        expect(response.code).to eq 200
+        expect(status).to eq 'ok'
+        expect(balance).to match(/^\$[0-9,]{1}/)
       end
+    end
   end
 end
