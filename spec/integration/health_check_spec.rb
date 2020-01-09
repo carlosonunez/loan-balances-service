8deec5a7a80a699ae2aa93208b079818c608ab54
Carlos Nunez <carlosonunez@noreply.github.com>
2020-01-08 23:17:58 -0600

    Use a function for interacting with endpoints

---
+++ spec/integration/health_check_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+describe 'Given a live Loan Balances service' do
+  context 'When I ping it' do
+    example 'Then it responds', :integration do
+      Service.get('/ping') do |response|
+        expect(response.code).to eq 200
+        expect(response.body).to eq({
+          status: 'ok',
+          message: "Hello from Carlos's Loan Balance service!"
+        }.to_json)
+      end
+    end
+  end
+end
