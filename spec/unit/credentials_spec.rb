da6eff8c1115e0112f6c0804365ff103ec9bf76b
Carlos Nunez <carlosonunez@noreply.github.com>
2020-01-08 23:48:09 -0600

    Start working on getting credentials fetching from DynamoDB.

---
+++ spec/unit/credentials_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Given a provider' do
+  context 'When we fetch credentials for it' do
+    example 'Then we get them as a hash' do
+    end
+  end
+end
